16ba605f9593872f95baabc80458ecca49ae8f0e
nanaym <59910434+nanaym@users.noreply.github.com>
2020-03-27 15:52:31 +0200

    postia aloitettu

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import { View } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
 import ExcistingDates from './components/ExcistingDates';
-import { View } from 'react-native';
+import PostNew from './components/PostNew';
 
 export default function App() {
   return (
     <View>
     <AppNavigator />
-    <ExcistingDates />
+    <PostNew />
     </View>
   )
 };
